ca9d41fc2a7b2e9cc37c16f30bd61f4ee941b1cf
parkjaewon <wodnjs3336@naver.com>
2024-01-16 18:32:39 +0900

    Delete ViewModel

--- app/src/main/java/com/example/spnapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/spnapplication/ui/dashboard/DashboardFragment.kt
@@ -22,16 +22,11 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
--- app/src/main/java/com/example/spnapplication/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/spnapplication/ui/notifications/NotificationsFragment.kt
@@ -22,16 +22,11 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
