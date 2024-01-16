ff761a99cbf208577be6e5f063b8483cdade131a
parkjaewon <wodnjs3336@naver.com>
2024-01-16 19:33:37 +0900

    Viewpager, Taplayout

--- app/src/main/java/com/example/spnapplication/MainActivity.kt
+++ app/src/main/java/com/example/spnapplication/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.example.spnapplication
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.example.spnapplication.databinding.ActivityMainBinding
+import com.example.spnapplication.ui.dashboard.DashboardFragment
+import com.example.spnapplication.ui.home.HomeFragment
+import com.example.spnapplication.ui.notifications.NotificationsFragment
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,18 +24,38 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViewPager()
+    }
+    private fun initViewPager() {
+        //ViewPager2 Adapter 셋팅
+        val viewPager2Adatper = ViewPager2Adapter(this)
+        viewPager2Adatper.addFragment(DashboardFragment())
+        viewPager2Adatper.addFragment(HomeFragment())
+        viewPager2Adatper.addFragment(NotificationsFragment())
+        viewPager2Adatper.addFragment(ContactFragment())
+        viewPager2Adatper.addFragment(MyPageFragment())
 
-        val navView: BottomNavigationView = binding.navView
+        //Adapter 연결
+        binding.vpViewpagerMain.apply {
+            adapter = viewPager2Adatper
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                }
+            })
+        }
+
+        //ViewPager, TabLayout 연결
+        TabLayoutMediator(binding.tlNavigationView, binding.vpViewpagerMain) { tab, position ->
+            Log.e("jblee", "ViewPager position: ${position}")
+            when (position) {
+                0 -> tab.text = "키패드"
+                1 -> tab.text = "연락처"
+                2 -> tab.text = "최근기록"
+                3 -> tab.text = "즐겨찾기"
+                4 -> tab.text = "내정보"
+            }
+        }.attach()
     }
 }
