59b1df3cf0a5a1a9d5ba82dfe9776947915c43e2
parkjaewon <wodnjs3336@naver.com>
2024-01-17 14:37:52 +0900

    design icon

--- app/src/main/java/com/example/spnapplication/MainActivity.kt
+++ app/src/main/java/com/example/spnapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.spnapplication
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
 import com.example.spnapplication.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayoutMediator
@@ -18,7 +20,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         initViewPager()
     }
+
     private fun initViewPager() {
+        val viewPager = binding.vpViewpagerMain
+        val tapLayout = binding.tlNavigationView
         //ViewPager2 Adapter 셋팅
         val viewPager2Adatper = ViewPager2Adapter(this)
         viewPager2Adatper.addFragment(ContactFragment())
@@ -30,13 +35,14 @@ class MainActivity : AppCompatActivity() {
         //Adapter 연결
         binding.vpViewpagerMain.apply {
             adapter = viewPager2Adatper
-
-            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-                override fun onPageSelected(position: Int) {
-                    super.onPageSelected(position)
-                }
-            })
         }
+        val iconList = ArrayList<Drawable?>()
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.circle))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.circle))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.circle))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.circle))
+        iconList.add(ContextCompat.getDrawable(this, R.drawable.circle))
+
 
         //ViewPager, TabLayout 연결
         TabLayoutMediator(binding.tlNavigationView, binding.vpViewpagerMain) { tab, position ->
@@ -47,6 +53,13 @@ class MainActivity : AppCompatActivity() {
                 2 -> tab.text = "최근기록"
                 3 -> tab.text = "즐겨찾기"
                 4 -> tab.text = "내정보"
+            }
+            when (position) {
+                0 -> tab.icon = iconList[position]
+                1 -> tab.icon = iconList[position]
+                2 -> tab.icon = iconList[position]
+                3 -> tab.icon = iconList[position]
+                4 -> tab.icon = iconList[position]
             }
         }.attach()
     }
