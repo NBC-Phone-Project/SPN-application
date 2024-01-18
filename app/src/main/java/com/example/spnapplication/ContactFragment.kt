d4a3681eb9005aee6fa8bff630f7d22919d7459b
YYG609 <yyg864@gmail.com>
2024-01-18 11:22:37 +0900

    feat: FloatingButton

--- app/src/main/java/com/example/spnapplication/ContactFragment.kt
+++ app/src/main/java/com/example/spnapplication/ContactFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AlphaAnimation
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.spnapplication.databinding.FragmentContactBinding
 
 class ContactFragment : Fragment() {
@@ -135,6 +137,31 @@ class ContactFragment : Fragment() {
         val adapter = UserAdapter(userList)
         binding?.rvContactRecyclerView?.adapter = adapter
         binding?.rvContactRecyclerView?.layoutManager = LinearLayoutManager(requireContext())
+
+
+        // 플로팅 버튼
+        val fadeIn = AlphaAnimation(0f, 1f).apply { duration = 500 }
+        val fadeOut = AlphaAnimation(1f, 0f).apply { duration = 500 }
+        var isTop = true
+        binding?.rvContactRecyclerView?.addOnScrollListener(object :RecyclerView.OnScrollListener(){
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (binding?.rvContactRecyclerView?.canScrollVertically(-1) == false && newState == RecyclerView.SCROLL_STATE_IDLE) {
+                    binding?.fbContactFloating?.startAnimation(fadeOut)
+                    binding?.fbContactFloating?.visibility = View.GONE
+                    isTop = true
+                } else {
+                    if (isTop){
+                        binding?.fbContactFloating?.visibility = View.VISIBLE
+                        binding?.fbContactFloating?.startAnimation(fadeIn)
+                        isTop = false
+                    }
+                }
+            }
+        })
+        binding?.fbContactFloating?.setOnClickListener {
+            binding?.rvContactRecyclerView?.smoothScrollToPosition(0)
+        }
 
         return binding?.root
     }
