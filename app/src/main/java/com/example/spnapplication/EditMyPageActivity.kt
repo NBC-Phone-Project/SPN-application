52003859cbde1c31869ad66b37be45f2879bc84f
Gahyeon Lee <igh9252@gmail.com>
2024-01-18 19:55:50 +0900

    내정보 편집창 + 버튼 재추가

--- app/src/main/java/com/example/spnapplication/EditMyPageActivity.kt
+++ app/src/main/java/com/example/spnapplication/EditMyPageActivity.kt
@@ -2,10 +2,33 @@ package com.example.spnapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.example.spnapplication.databinding.ActivityEditMyPageBinding
 
 class EditMyPageActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityEditMyPageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_my_page)
+        setContentView(binding.root)
+
+        binding.run {
+            icMypageBack.setOnClickListener {
+                val myPhoneNumber = binding.etMyPhoneNumber.text.toString()
+                val myEmail = binding.etMyEmail.text.toString()
+                val myMemo = binding.etMyMemo.text.toString()
+                val data = MyPageFragment.newInstance(myPhoneNumber,myEmail,myMemo)
+                setFragment(data)
+            }
+        }
+    }
+
+    private fun setFragment(frag : Fragment) {
+        supportFragmentManager.commit {
+            replace(R.id.vp_viewpager_main, frag)
+            setReorderingAllowed(true)
+            addToBackStack("")
+        }
     }
 }
--- app/src/main/java/com/example/spnapplication/MyPageFragment.kt
+++ app/src/main/java/com/example/spnapplication/MyPageFragment.kt
@@ -1,25 +1,39 @@
 package com.example.spnapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
+import com.example.spnapplication.databinding.FragmentMyPageBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MyPageFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val ARG_PRAM1 = "param1"
+private const val ARG_PRAM2 = "param2"
+private const val ARG_PRAM3 = "param3"
 class MyPageFragment : Fragment() {
-    // TODO: Rename and change types of parameters
+    private val binding by lazy { FragmentMyPageBinding.inflate(layoutInflater) }
     private var param1: String? = null
     private var param2: String? = null
+    private var param3: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
+            param1 = it.getString(ARG_PRAM1)
+            param2 = it.getString(ARG_PRAM2)
+            param3 = it.getString(ARG_PRAM3)
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.tvMyPhoneNumberData.text = param1
+        binding.tvMyEailData.text = param2
+        binding.tvMyMemoData.text = param3
     }
 
     override fun onCreateView(
@@ -31,20 +45,14 @@ class MyPageFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyPageFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(param1: String, param2: String, param3: String) =
             MyPageFragment().apply {
                 arguments = Bundle().apply {
-
+                    putString(ARG_PRAM1, param1)
+                    putString(ARG_PRAM2, param2)
+                    putString(ARG_PRAM3, param3)
                 }
             }
     }
